9095e7e878f0d0997df7fa4fbe173d862d5a74a3
charlotte45 <charlotte.prieur@yahoo.fr>
2017-12-03 11:57:12 +0100

    quete form

--- src/Wcs/CoavBundle/Form/ReviewType.php
+++ src/Wcs/CoavBundle/Form/ReviewType.php
@@ -2,22 +2,54 @@
 
 namespace Wcs\CoavBundle\Form;
 
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ReviewType extends AbstractType
 {
     /**
-     * {@inheritdoc}
+     * {@inheritdoc} Including all fields from Review entity.
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('text')->add('publicationDate')->add('note')->add('userRated')->add('reviewAuthor');
+        $builder
+            ->add('text', TextareaType::class, array(
+                'attr' => array(
+                    'maxlength' => 250,
+                    'label' => 'Description'
+            )))
+            ->add('publicationDate', DateType::class, array(
+                'data' => new \DateTime('now'
+                )))
+            ->add('note', IntegerType::class, array(
+                'attr' => array(
+                    'min' => 0,
+                    'max' => 5,
+                    'label' => 'Note'
+                )))
+            ->add('agreeTerms', CheckboxType::class, array(
+                'mapped' => false
+            ))
+            ->add('userRated', EntityType::class, array(
+                'class' => 'Wcs\CoavBundle\Entity\User',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.lastName', 'ASC');
+                },
+                'choice_label' => 'phoneNumber'
+            ))
+            ->add('reviewAuthor');
     }
     
     /**
-     * {@inheritdoc}
+     * {@inheritdoc} Targeting Review entity
      */
     public function configureOptions(OptionsResolver $resolver)
     {
@@ -27,7 +59,7 @@ class ReviewType extends AbstractType
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritdoc} getName() is now deprecated
      */
     public function getBlockPrefix()
     {
